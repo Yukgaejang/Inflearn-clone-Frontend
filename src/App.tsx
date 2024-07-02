a8065f3e078d1f9285036103ab197b2b22b97274
minsun <elinnane0@gmail.com>
2024-07-02 14:47:37 +0900

    fix: app.tsx 수정 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Post from "./pages/Post";
 
 const App: React.FC = () => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
 
   return (
   <div>
-    <Post/>
   </div>
 );
 }
