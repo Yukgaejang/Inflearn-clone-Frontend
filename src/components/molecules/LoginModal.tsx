251a894ef2c195555651182ed427dbbf0e6a61f7
SemiHyeon <gustpal08@gmail.com>
2024-07-05 07:07:06 +0900

    feat: 로그인 페이지 생성

---
+++ src/components/molecules/LoginModal.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "../../styles/Board.css";
+import ModalBox from "../atoms/ModalBox";
+import SingUpSocial from "./SingUp-Social";
+import '../../styles/SingUp.css';
+import CloseIcon from '@mui/icons-material/Close';
+
+
+interface ProblemModalProps {
+    open: boolean;
+    setOpen: (open: boolean) => void;
+}
+
+const ProblemModal: React.FC<ProblemModalProps> = ({ open, setOpen }) => {
+    const handleClose = () => {
+        setOpen(false);
+    };
+    return (
+        <>
+            <ModalBox
+                type="login"
+                size="small"
+                head="로그인"
+                body=""
+                open={open}
+                setOpen={setOpen}
+            >
+                <button className="close-button" onClick={handleClose}>
+                    <CloseIcon/>
+                </button>
+
+                <div className="singUpTemplates-line">
+                    <p>간편로그인</p>
+                </div>
+
+                <SingUpSocial/>
+            </ModalBox>
+        </>
+
+    );
+};
+
+export default ProblemModal;
