eaf9345fdc6814d997e9e65ff7f4fde384586d67
SemiHyeon <gustpal08@gmail.com>
2024-07-05 05:45:12 +0900

    feat: 회원가입 페이지 생성

---
+++ src/pages/SignUpPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignUpPageTemplates from '../components/templates/SingUpTemplates';
+import '../styles/SingUp.css'
+
+const SignUpPage = () => {
+    return (
+        <div className="SignUpPage">
+            <SignUpPageTemplates />
+        </div>
+    );
+};
+
+export default SignUpPage;
